78c6a87504f3b7dbdcfd9f3bf89a87177f2bd908
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2012-09-18 17:08:01 +0200

    * Added more Table stuff.

--- chap01/interp.c
+++ chap01/interp.c
@@ -1,10 +1,37 @@
+#include <string.h>
 #include "slp.h"
+#include "interp.h"
+#include "util.h"
+
+Table_  Table(string id, int value, struct table *tail) {
+  Table_ t = malloc(sizeof(*t));
+  t->id = id;
+  t->value = value;
+  t->tail = tail;
+  return t;
+}
 
 void interp(A_stm stm) {
 }
 
-void interpStm(A_stm stm) {
+Table_ interpStm(A_stm s, Table_ t) {
+  return NULL;
 }
 
-void interpExp(A_exp exp) {
+struct IntAndTable interpExp(A_exp e, Table_ t) {
+  ;
+}
+
+Table_ update(Table_ t, string id, int value) {
+  Table_ newElement = Table(id, value, t);
+  return newElement;
+}
+
+int lookup(Table_ t, string key) {
+  while (t != NULL) {
+    if (strcmp(t->id, key) == 0) {
+      return t->value;
+    }
+  }
+  return 1; // TODO: can this happen???
 }
--- chap01/interp.h
+++ chap01/interp.h
@@ -5,28 +5,51 @@
 #include "util.h"
 
 typedef struct table *Table_;
-
 struct table {
   string id;
   int value;
   Table_ tail;
 };
+Table_  Table(string id, int value, struct table *tail);
 
-Table_ Table(string id, int value, struct table *tail) {
-  Table_ t = malloc(sizeof(*t));
-  t->id = id;
-  t->value = value;
-  t->tail = tail;
-  return t;
-}
+/*
+ * Interpreting expressions is more complicated than interpreting statements,
+ * because expressions return integer values AND have side effects.  We wish
+ * to simulate the straight-line programming language's assignment statements
+ * without doing any side effects in the interpreter itself. (The print
+ * statements will be accomplished by interpreter side effects, however.)  Part
+ * of the solution for this is to use this struct.
+ */
+struct IntAndTable {
+  int i;
+  Table_ t;
+};
 
 /*
  * "Interpret" a program in this language.
  */
 void interp(A_stm stm);
 
-void interpStm(A_stm stm); // must be recursive!
+/*
+ * Produce a new table from the specified table.  The new table is just like
+ * the original one except that some identifiers map to different integers as
+ * a result of the given statement.
+ */
+Table_ interpStm(A_stm s, Table_ t);
 
-void interpExp(A_exp exp); // must be recursive!
+/*
+ * The result of interpreting an expression e1 with table t1 is an integer
+ * value i and a new table t2.  When interpreting an expression with two
+ * subexpressions (such as an OpExp), the table t2 resulting from the first
+ * subexpression can be used in processing the second subexpression.
+ */
+struct IntAndTable interpExp(A_exp e, Table_ t);
+
+/*
+ * Put a new element at the head of the linked list.
+ */
+Table_ update(Table_ t, string id, int value);
+
+int lookup(Table_ t, string key);
 
 #endif /* INTERP_H */
